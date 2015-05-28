955b46a085e184229ce8e541a45f5f99ef68b144
clin <orzgodlo@gmail.com>
2015-05-28 02:03:00 -0700

    WIP: dump block_based_table

--- testing/dump_sst_file/sst_dump.cc
+++ testing/dump_sst_file/sst_dump.cc
@@ -6,13 +6,20 @@
 #include <string>
 
 #include "util/sst_dump_tool_imp.h"
+#include "table/block_based_table_reader.h"
 
 using namespace rocksdb;
 
 
-int main() {
-    SstFileReader sst_reader("sst.txt", true, true);
-    sst_reader.DumpTable("dump.txt");
+int main(int argc, char** argv) {
+    if(argc != 3) {
+        printf("argc = %d\n", argc);
+        printf("./sst_dump <sst_file_name> <dump_file_name>\n");
+        exit(1);
+    }
+
+    SstFileReader sst_reader(argv[1], true, true);
+    sst_reader.DumpTable(argv[2]);
 
     return 0; 
 }
